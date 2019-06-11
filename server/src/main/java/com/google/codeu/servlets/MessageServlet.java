1418c543bffc01f3462e6350ca93f47a58219a06
Jordan Barton <jordanbarton@Jordans-MacBook-Air.local>
2019-06-11 16:21:41 -0400

    Added code to allow users to make bolded messages

--- server/src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ server/src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -74,10 +74,19 @@ public class MessageServlet extends HttpServlet {
     }
 
     String user = userService.getCurrentUser().getEmail();
+    //Disallows any HTML content
     String text = Jsoup.clean(request.getParameter("text"), Whitelist.none());
-
     String userText = Jsoup.clean(request.getParameter("text"), Whitelist.none());
 
+
+    //Replacing Info
+    String parsedText = userText.replace("(bold)", "<strong>").replace("(/bold)", "</strong>");
+    //make sure generated HTML is valid and all tags are closed
+    String cleanedContent = Jsoup.clean(parsedText, Whitelist.none().addTags("strong"));
+		//replacing text in textbox with cleaned content
+    userText = cleanedContent;
+
+   
     String regex = "(https?://\\S+\\.(png|jpg))";
     String replacement = "<img src=\"$1\" />";
     String textWithImagesReplaced = userText.replaceAll(regex, replacement);
